533c4c2b6ac50defae3e9059436e4e992357fcb6
JonelleSAllen <jonellesallen@students.utech.edu.jm>
2025-11-24 15:12:59 -0500

    NotificationService.h

--- NotificationService.h
+++ NotificationService.h
@@ -1,1 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Notification_Service.h"
 
+Class NotificationService {
+public:
+  virtual ~NotificationService() = default;
+  virtual bool connectToProvider(const std::string& provider) = 0;
+  virtual bool send(const std::string& recipient, const std::string& message) = 0
+};
