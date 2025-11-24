5dc8a55b4cd21aa535d3f027cf6700b866ca6882
JonelleSAllen <jonellesallen@students.utech.edu.jm>
2025-11-24 14:56:10 -0500

    Update Permission.h

--- Permission.h
+++ Permission.h
@@ -1,1 +1,22 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Permission.h"
+
+class Permission {
+private: 
+std:: string permission;
+stdd:: string description;
+
+public:
+Permission () = default ;
+Permission (const std::string& perm, const std::string& desc)
+: permission(perm) , description(desc){}
+
+std::string getPermission() const {return permission; }
+std::string getDescription() const {return description} 
+
+void setPermission(const std::string& perm) {permission = perm }
+void setDescription(const std::string& desc) {description = desc}
+;
 
